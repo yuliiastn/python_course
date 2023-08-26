eaac8232422fc871ff98d332df743b6398e868c3
Yuliia <142430440+yuliiastn@users.noreply.github.com>
2023-08-26 14:21:16 +0100

    HW10

---
+++ l10_task2.py
@@ -0,0 +1,12 @@
+with open('lorem_ipsum.txt', 'w') as file1:
+    file1.writelines(
+        'Lorem ipsum dolor sit amet, '
+        'consectetur adipiscing elit, sed do eiusmod '
+        'tempor incididunt ut labore et dolore magna aliqua.'
+        )
+
+with open('lorem_ipsum.txt') as file1, open('lorem_copy.txt', 'w') as file2:
+    line = file1.readline()
+    file2.write(line.upper())
+
+    
