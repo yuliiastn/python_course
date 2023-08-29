fd6f8483bdfcc91a507e2cde36b08f4be35e6d83
Yuliia <142430440+yuliiastn@users.noreply.github.com>
2023-08-29 11:18:19 +0100

    Update l10_task2.py

--- l10_task2.py
+++ l10_task2.py
@@ -6,7 +6,7 @@ with open('lorem_ipsum.txt', 'w') as file1:
         )
 
 with open('lorem_ipsum.txt') as file1, open('lorem_copy.txt', 'w') as file2:
-    line = file1.readline()
+    line = file1.read()
     file2.write(line.upper())
 
     
