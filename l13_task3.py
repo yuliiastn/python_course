2b453708a737e52a1ef33420e876e9875790aa60
Yuliia <142430440+yuliiastn@users.noreply.github.com>
2023-09-07 14:52:38 +0100

    revised l13_task3.py

--- l13_task3.py
+++ l13_task3.py
@@ -11,30 +11,18 @@ class Car:
         if speed < 0:
             raise ValueError('Speed cannot be a negative number')
 
-
     def accelerate(self):
-        return self.speed + 5
-
+        self.speed = self.speed + 5
+        return self.speed
 
     def brake(self):
-        if self.speed <= 5:
-            self.speed = self.speed - self.speed
-        else:
-            self.speed = self.speed - 5
+        self.speed = max(self.speed - 5, 0)
         return self.speed
         
-
     def display_speed(self):
         return f'The speed is {self.speed}'
-    
+
 
 Audi = Car('A1', 2020, 5)
 
 print(Audi.accelerate())
-
-
-
-
-
-
-
