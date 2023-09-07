50e1504b05d1ce9093fe58adef2c8358aeaeb374
Yuliia <142430440+yuliiastn@users.noreply.github.com>
2023-09-07 14:41:26 +0100

    l13_task1.py

--- l13_task1.py
+++ l13_task1.py
@@ -8,13 +8,13 @@ class Country:
             f'{self.name} {other.name}',
             self.population + other.population
         )
-
+        
+    def __str__(self):
+        return f'Name: {self.name}; ' \
+                f'Population: {self.population:_}'
 
 bosnia = Country('Bosnia', 10_000_000)
 herzegovina = Country('Herzegovina', 5_000_000)
 
 bosnia_herzegovina = bosnia.add(herzegovina)
-print(
-    f"Name: {bosnia_herzegovina.name}, "
-    f"Population: {bosnia_herzegovina.population:_}"
-    )
+print(bosnia_herzegovina)
